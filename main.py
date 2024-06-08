721dc43dbf45fc8ed131653b0afdae89786c8204
ecarnovsky <ecarnovsky@gmail.com>
2024-06-08 14:35:35 -0400

    the sale price is now extracted from the page

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import NoSuchElementException
+
+urls = ['https://www.meijer.com/shopping/product/boston-warehouse-figural-mug-burger-18oz/2660220593.html',
+        'https://www.meijer.com/shopping/product/lindt-lindor-assorted-chocolate-candy-truffles-chocolates-with-smooth-melting-truffle-center-15-2-oz-bag/954201519.html']
 
 print("Starting...")
 
 
 driver = uc.Chrome()
-driver.get('https://www.meijer.com/shopping/product/boston-warehouse-figural-mug-burger-18oz/2660220593.html')
-driver.implicitly_wait(15)
+
+for url in urls:
+    
+    try:
+        driver.get(url)
+        driver.implicitly_wait(15)
+        sale_price = driver.find_element(by=By.CSS_SELECTOR, value="span.product-info__sale-price>span").text
+        print("The item is on sale for: " + sale_price)
+    except NoSuchElementException as e:
+        print("The item is not on sale.")
+    except WebDriverException as e:
+        print("A webdriver error has occurred. This can be caused by being unable to connect to the website.")
+
+
 driver.close()
 time.sleep(1)
