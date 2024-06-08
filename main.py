279b1e87e4dc18473c5a1b7a0a7ebff3c1f4037b
ecarnovsky <ecarnovsky@gmail.com>
2024-06-08 14:36:47 -0400

    Added error messages to the print statements

--- main.py
+++ main.py
@@ -13,16 +13,20 @@ print("Starting...")
 driver = uc.Chrome()
 
 for url in urls:
-    
+
     try:
         driver.get(url)
         driver.implicitly_wait(15)
         sale_price = driver.find_element(by=By.CSS_SELECTOR, value="span.product-info__sale-price>span").text
         print("The item is on sale for: " + sale_price)
+
+        item_name = "name"
+        item_regular_price = 1
+
     except NoSuchElementException as e:
-        print("The item is not on sale.")
+        print("The item is not on sale.\n Error Message: " + e)
     except WebDriverException as e:
-        print("A webdriver error has occurred. This can be caused by being unable to connect to the website.")
+        print("A webdriver error has occurred. This can be caused by being unable to connect to the website.\n Error Message: " + e)
 
 
 driver.close()
