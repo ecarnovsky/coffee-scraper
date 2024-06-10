bd0213e4d99d43e24888a676c6abc6b1d0b64653
ecarnovsky <ecarnovsky@gmail.com>
2024-06-10 16:38:20 -0400

    Set up the ability to send emails

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import NoSuchElementException
+from sale_item import SaleItem
 
 urls = []
+sale_items = []
 
 
 print("Application starting...")
@@ -38,8 +40,10 @@ for url in urls:
         sale_price = driver.find_element(by=By.CSS_SELECTOR, value="span.product-info__sale-price>span").text
         print("The item is on sale for: " + sale_price)
 
-        item_name = "name"
-        item_regular_price = 1
+        name = "name"
+        original_price = 1
+        sale_item = SaleItem(name, sale_price, original_price, url)
+        sale_items.append(sale_item)
 
     except NoSuchElementException as e:
         print("Could not locate sale price element. Most likely the item is not on sale. \n" + str(e))
---
+++ sale_item.py
@@ -0,0 +1,6 @@
+class SaleItem: 
+    def __init__(self, name, sale_price, original_price, url):
+        self.name = name 
+        self.sale_price = sale_price
+        self.original_price = original_price
+        self.url = url
