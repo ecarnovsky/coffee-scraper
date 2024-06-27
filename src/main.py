d647a57bdf920a3c2d61a787f1af6c239a8998e1
ecarnovsky <ecarnovsky@gmail.com>
2024-06-27 15:09:59 -0400

    Added main and get_urls_from_file functions

--- src/main.py
+++ src/main.py
@@ -5,53 +5,61 @@ from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import NoSuchElementException
 from sale_item import SaleItem
 
-urls = []
-sale_items = []
+def main():
+
+    urls = get_urls_from_file("src/urls/new_urls.txt")
+    sale_items = []
+
+    print("Application starting...")
 
 
-print("Application starting...")
+    driver = uc.Chrome()
+
+    for url in urls:
+
+        try:
+            driver.get(url)
+            driver.implicitly_wait(15)
+            sale_price = driver.find_element(by=By.CSS_SELECTOR, value="span.product-info__sale-price>span").text
+            print("The item is on sale for: " + sale_price)
+
+            name = "name"
+            original_price = 1
+            sale_item = SaleItem(name, sale_price, original_price, url)
+            sale_items.append(sale_item)
+
+        except NoSuchElementException as e:
+            print("Could not locate sale price element. Most likely the item is not on sale. \n" + str(e))
+        except WebDriverException as e:
+            print("A webdriver error has occurred. This can be caused by being unable to connect to the website.\n" + str(e))
 
 
-new_urls_file = open('src/urls/new_urls.txt', 'r')
+    print("Application ending...")
 
-while True:
-
-    url = new_urls_file.readline()
-
-    if not url:
-        break
-    else:
-        urls.append(url)
+    driver.close()
+    time.sleep(1)
 
 
-if len(urls) == 0:
-    exit()
+def get_urls_from_file(path):
+
+    urls = []
+
+    new_urls_file = open(path, 'r')
+
+    while True:
+
+        url = new_urls_file.readline()
+
+        if not url:
+            break
+        else:
+            urls.append(url)
 
 
+    if len(urls) == 0:
+        exit()
+
+    return urls
 
 
-driver = uc.Chrome()
-
-for url in urls:
-
-    try:
-        driver.get(url)
-        driver.implicitly_wait(15)
-        sale_price = driver.find_element(by=By.CSS_SELECTOR, value="span.product-info__sale-price>span").text
-        print("The item is on sale for: " + sale_price)
-
-        name = "name"
-        original_price = 1
-        sale_item = SaleItem(name, sale_price, original_price, url)
-        sale_items.append(sale_item)
-
-    except NoSuchElementException as e:
-        print("Could not locate sale price element. Most likely the item is not on sale. \n" + str(e))
-    except WebDriverException as e:
-        print("A webdriver error has occurred. This can be caused by being unable to connect to the website.\n" + str(e))
-
-
-print("Application ending...")
-
-driver.close()
-time.sleep(1)
+main()
